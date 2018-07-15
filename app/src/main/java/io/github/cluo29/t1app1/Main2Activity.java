f8391815398a95e5c888c402919b88d0a9f6c124
Chu Luo <mr.cluo29@gmail.com>
2018-07-15 19:31:42 +1000

    Initial commit

---
+++ app/src/main/java/io/github/cluo29/t1app1/Main2Activity.java
@@ -0,0 +1,17 @@
+package io.github.cluo29.t1app1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Log.d("MyApp","I am Activity 2");
+
+    }
+}
---
+++ app/src/main/java/io/github/cluo29/t1app1/MainActivity.java
@@ -0,0 +1,44 @@
+package io.github.cluo29.t1app1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.buttonAct1);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+        public void onClick(View v) {
+                // move from ActivityOne to ActivityTwo
+                Intent intent = new Intent(MainActivity.this,
+                        Main2Activity.class);
+                startActivity(intent);    }});
+
+
+
+        int intA = 0;
+        int intB = 1;
+        Log.d("MyApp","value of A + B =" + (intA+intB));
+
+
+
+        Log.d("MyApp","line number =" + 18);
+
+
+
+
+    }
+}
